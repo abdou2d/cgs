8fc4dd1970450a272f36ffaf46a3b71d1f5d1bec
abdou2d <abdou2d@gmail.com>
2020-07-27 10:46:40 +0200

    tr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "cgs_production"
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
