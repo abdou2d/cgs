0213e7af97fa9faa9c3bace129dc8d40a9aec6f7
abdou2d <abdou2d@gmail.com>
2020-07-27 10:44:05 +0200

    se

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey',
-    :password => ENV["SEND_KEY"],
+    :password => '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',
     :domain => 'cganatomy.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
