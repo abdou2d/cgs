6c75c1fadba053a12c0c5abea96fe0c83c0bc0d7
abdou2d <abdou2d@gmail.com>
2020-07-28 04:41:57 +0200

    zoho

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,8 +1,8 @@
-ActionMailer::Base.smtp_settings = {
-    :address => ENV["SMTP_SERVER"],
+ActionMailer::Base.smtp_settings = { 
+    :address => "smtp.zoho.com",
     :port => 587,
-    :user_name => ENV["SMTP_USERNAME"], #Your SMTP user
-    :password => ENV["SMTP_PASSWORD"], #Your SMTP password
+    :user_name => 'mail@cganatomy.com',
+    :password => ENV["SMTP_ZOHO_PASSWORD"],
     :authentication => :login,
     :enable_starttls_auto => true
   }
